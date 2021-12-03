49e6d52ed42e1fb261c73beb529a93ba797e0011
Shohei YOSHIDA <syohex@gmail.com>
2021-12-03 12:43:19 +0900

    Add Day01 part2

--- advent_of_code/2021/day01/src/main.rs
+++ advent_of_code/2021/day01/src/main.rs
@@ -19,6 +19,25 @@ fn count_increased(nums: &Vec<i32>) -> i32 {
     count
 }
 
+fn count_increased_three_pair(nums: &Vec<i32>) -> i32 {
+    if nums.is_empty() {
+        return 0;
+    }
+
+    let mut count = 0;
+    let mut prev = nums[0] + nums[1] + nums[2];
+    for i in 3..nums.len() {
+        let sum = nums[i] + nums[i - 1] + nums[i - 2];
+        if sum > prev {
+            count += 1;
+        }
+
+        prev = sum;
+    }
+
+    count
+}
+
 fn main() {
     let file = fs::File::open("data/input.txt").unwrap();
     let lines = io::BufReader::new(file).lines();
@@ -33,12 +52,16 @@ fn main() {
         .collect::<Vec<i32>>();
 
     let part1 = count_increased(&nums);
+    let part2 = count_increased_three_pair(&nums);
     assert_eq!(part1, 1448);
+    assert_eq!(part2, 1471);
     println!("Part1: {}", part1);
+    println!("Part2: {}", part2);
 }
 
 #[test]
 fn test_count_increased() {
     let nums = vec![199, 200, 208, 210, 200, 207, 240, 269, 260, 263];
     assert_eq!(count_increased(&nums), 7);
+    assert_eq!(count_increased_three_pair(&nums), 5);
 }
