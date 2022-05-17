9a6addda5f82a4cd7c7e5c2399c1f92c6f4aabf3
Shohei YOSHIDA <syohex@gmail.com>
2022-05-18 00:05:53 +0900

    Add handle inconsistent dates

---
+++ rust_code_challenges/handle_inconsistent_dates/src/main.rs
@@ -0,0 +1,63 @@
+use chrono::NaiveDate;
+
+/// Parses a string that represents a date. When a date
+/// is unable to be determined, return `None`.
+fn flexible_date_parse(text: &str) -> Option<NaiveDate> {
+    NaiveDate::parse_from_str(text, "%Y-%m-%d")
+        .or_else(|_| NaiveDate::parse_from_str(text, "%Y/%b/%d"))
+        .or_else(|_| NaiveDate::parse_from_str(text, "%Y/%b/%d"))
+        .or_else(|_| NaiveDate::parse_from_str(text, "%d.%b.%Y"))
+        .or_else(|_| NaiveDate::parse_from_str(text, "%B.%d.%Y"))
+        .ok()
+}
+
+fn main() {
+    let dates = [
+        "2010-12-11",
+        "1999/Mar/02",
+        "01.Mar.2021",
+        "Mar.05.2021",
+        "not a date",
+    ];
+
+    for d in dates.iter() {
+        println!("{} -> {:?}", d, flexible_date_parse(d));
+    }
+}
+
+#[test]
+fn ymd_hyphen() {
+    assert_eq!(
+        flexible_date_parse("2010-12-11"),
+        Some(NaiveDate::from_ymd(2010, 12, 11))
+    )
+}
+
+#[test]
+fn ymd_slash() {
+    assert_eq!(
+        flexible_date_parse("1999/Mar/02"),
+        Some(NaiveDate::from_ymd(1999, 3, 2))
+    )
+}
+
+#[test]
+fn dmy_dot() {
+    assert_eq!(
+        flexible_date_parse("01.Mar.2021"),
+        Some(NaiveDate::from_ymd(2021, 3, 1))
+    )
+}
+
+#[test]
+fn mdy_dot() {
+    assert_eq!(
+        flexible_date_parse("Apr.05.2021"),
+        Some(NaiveDate::from_ymd(2021, 4, 5))
+    )
+}
+
+#[test]
+fn invalid() {
+    assert_eq!(flexible_date_parse("not a date"), None)
+}
